5166396cf74af91348a5d291414e66ab2a01caee
WagnerLeodoro <wagner.augustoleodoro@gmail.com>
2024-02-26 22:34:16 -0300

    Cadastro de notas - page New

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,7 @@
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { api } from '../../services/api'
+
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { Section } from '../../components/Section'
@@ -5,20 +9,46 @@ import { Button } from '../../components/Button'
 import { Container, Form } from './style'
 import { Textarea } from '../../components/Textarea'
 import { NoteItem } from '../../components/NoteItem'
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
 
 export function New() {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
   const [links, setLinks] = useState([])
   const [newLink, setNewLink] = useState('')
+
+  const [tags, setTags] = useState([])
+  const [newTag, setNewTag] = useState('')
+
+  const navigate = useNavigate()
 
   function handleAddLink() {
     setLinks((prevState) => [...prevState, newLink])
     setNewLink('')
   }
 
+  function handleAddTag() {
+    setTags((prevState) => [...prevState, newTag])
+    setNewTag('')
+  }
+
   function handleRemoveLink(deleted) {
     setLinks((prevState) => prevState.filter((link) => link !== deleted))
+  }
+
+  function handleRemoveTag(deleted) {
+    setTags((prevState) => prevState.filter((tag) => tag !== deleted))
+  }
+
+  async function handleNewNote() {
+    await api.post('/notes', {
+      title,
+      description,
+      tags,
+      links,
+    })
+    alert('Nota criada com sucesso!')
+    navigate('/')
   }
   return (
     <Container>
@@ -31,8 +61,14 @@ export function New() {
             <Link to="/">voltar</Link>
           </header>
 
-          <Input placeholder="Título" />
-          <Textarea placeholder="Observações" />
+          <Input
+            placeholder="Título"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <Textarea
+            placeholder="Observações"
+            onChange={(e) => setDescription(e.target.value)}
+          />
 
           <Section title="Links Úteis">
             {links.map((link, index) => (
@@ -53,11 +89,25 @@ export function New() {
 
           <Section title="Marcadores">
             <div className="tags">
-              <NoteItem value="react" />
-              <NoteItem isnew placeholder="Nova tag" />
+              {tags.map((tag, index) => (
+                <NoteItem
+                  key={index}
+                  value={tag}
+                  onClick={() => {
+                    handleRemoveTag(tag)
+                  }}
+                />
+              ))}
+              <NoteItem
+                isnew
+                placeholder="Nova tag"
+                onChange={(e) => setNewTag(e.target.value)}
+                value={newTag}
+                onClick={handleAddTag}
+              />
             </div>
           </Section>
-          <Button title="Salvar" />
+          <Button title="Salvar" onClick={handleNewNote} />
         </Form>
       </main>
     </Container>
